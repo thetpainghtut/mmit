ce1419fc5d399427963543387bbe265eb9e8cb29
Ya Thaw Myat Noe <yathawmyatnoe007@gmail.com>
2019-04-07 23:00:45 +0630

    Resource and api route

--- app/Http/Resources/CityResource.php
+++ app/Http/Resources/CityResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Model\City;
 
 class CityResource extends JsonResource
 {
@@ -14,6 +15,11 @@ class CityResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,57 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // City
 Route::resource('/city', 'Api\CityController');
+
+// Township
+Route::resource('/township', 'Api\TownshipController');
+
+// Position
+Route::resource('/city', 'Api\PositionController');
+
+// Location
+Route::resource('/city', 'Api\LocationController');
+
+// Expense
+Route::resource('/city', 'Api\ExpenseController');
+
+// Income
+Route::resource('/city', 'Api\IncomeController');
+
+// Course
+Route::resource('/city', 'Api\CourseController');
+
+// Duration
+Route::resource('/city', 'Api\DurationController');
+
+// Staff
+Route::resource('/city', 'Api\StaffController');
+
+// Teacher
+Route::resource('/city', 'Api\TeacherController');
+
+// Section
+Route::resource('/city', 'Api\SectionController');
+
+// Company
+Route::resource('/city', 'Api\CompanyController');
+
+// Inquire
+Route::resource('/city', 'Api\InquireController');
+
+// Student
+Route::resource('/city', 'Api\StudentController');
+
+// Jobcareer
+Route::resource('/city', 'Api\JobcareerController');
+
+// Interview
+Route::resource('/city', 'Api\InterviewController');
+
+// Hire
+Route::resource('/city', 'Api\HireController');
+
+// Dismiss
+Route::resource('/city', 'Api\DismissController');
+
+// Attendance
+Route::resource('/city', 'Api\AttendanceController');
